6ba58862baac812b74b67e624eeabe1172f27c43
ThomasMalory <d.silvitelli@studenti.unisa.it>
2020-12-20 11:22:41 +0100

    commit18dicembre

--- Esercizio2Finale/CurrencyDynamic.py
+++ Esercizio2Finale/CurrencyDynamic.py
@@ -1,21 +1,30 @@
 from Esercizio2Intracorso.Currency import Currency
+import decimal
 
 # r = amount
 # curr.numDenominations()
 def dynamic(curr, r):
-    # initializing matrix 'a'
-    adjust = []
-    #if isinstance(r,float):
+    d = decimal.Decimal(str(r))
+    var = d.as_tuple().exponent
+    if var < -2:
+        return "value with over two decimal points are not allowed"
 
+    r = int(r * 100)
+
+    # initializing matrix 'a'
     a = [[1] * (r+1) for _ in range(0, curr.numDenominations())]
 
     it = curr.iterDenominations(False)
     coins = []
     for k in it:
-        coins.append(k*100)
+        d = decimal.Decimal(str(k))
+        var = d.as_tuple().exponent
+        if var < -2:
+            return "value with over two decimal points are not allowed"
+        coins.append(int(k * 100))
 
     for i in range(0, curr.numDenominations()):
-        for j in range(1, r+1):
+        for j in range(1,r+1):
             if i == 0:
                 if j % coins[0] == 0:
                     a[0][j] = 1
@@ -36,25 +45,17 @@ def dynamic(curr, r):
 curr = Currency("EUR")
 
 # ----------CURRENCY CONSTRUCTION OBJECT----------#
-# curr.AddDenomination(0.05)
-# curr.AddDenomination(0.1)
-# curr.AddDenomination(0.2)
-# curr.AddDenomination(0.5)
-# curr.AddDenomination(1)
-# curr.AddDenomination(2)
-# curr.AddDenomination(5)
-# curr.AddDenomination(10)
-# curr.AddDenomination(20)
-# curr.AddDenomination(50)
-# curr.AddDenomination(100)
-# curr.AddDenomination(200)
-# curr.AddDenomination(500)
-curr.AddDenomination(2)
-curr.AddDenomination(3)
-curr.AddDenomination(5)
-curr.AddDenomination(10)
+curr.AddDenomination(0.02)
+curr.AddDenomination(0.03)
+curr.AddDenomination(0.05)
+curr.AddDenomination(0.1)
 
-r = 15
+r = 0.15
+
 a = dynamic(curr, r)
+print("  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5]\n")
+appoggio = [2,3,5,1]
+j=0
 for i in a:
-    print(i)
+    print(appoggio[j], i)
+    j+=1
